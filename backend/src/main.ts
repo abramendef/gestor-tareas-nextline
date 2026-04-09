df6b2018066cb36f98d70a6f86d7b7e351103edf
JESUS ABRAHAM MENDEZ FIGUEROA <2024030405@upsin.edu.mx>
2026-04-09 13:01:27 -0700

    chore: habilitar cors para el frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Habilitamos CORS para que el frontend de Vue (en otro puerto) tenga permiso de hacer peticiones
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
